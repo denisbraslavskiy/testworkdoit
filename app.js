cb0a22c32256235503d3a27b9bc8580deb80ee18
Denis Braslavskiy <Denis Braslavskiy>
2016-02-18 17:47:29 +0200

    for openshift

--- app.js
+++ app.js
@@ -36,19 +36,19 @@ app.use(function(err, req, res, next) {
 app.use('/', routes);
 //app.use('/users', users);
 
-// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-//
-// server.listen(server_port, server_ip_address, function () {
-//   console.log( "Listening on " + server_ip_address + ", server_port " + port )
-// });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
 
 
- app.set('port', process.env.PORT || 3003);
-
- var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
- });
+ // app.set('port', process.env.PORT || 3003);
+ //
+ // var server = app.listen(app.get('port'), function() {
+ //  console.log('Express server listening on port ' + server.address().port);
+ // });
 //
 // module.exports = app;
 
